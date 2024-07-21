96795d925b3c6003b7d32b6084876dd2f83e683a
JakeIsKale <jakeclarkthompson@gmail.com>
2024-07-22 08:53:36 +1200

    Adding movement to Bad Guy

--- Godot - Game/Badguy.gd
+++ Godot - Game/Badguy.gd
@@ -1,8 +1,18 @@
 extends CharacterBody2D
 
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var player 
 
-
+func _physics_process(delta):
+	#Gravity for Frog
+	velocity.y += gravity * delta
+	
+	move_and_slide()
 
 func _on_player_detection_body_entered(body):
 	if body.name == "Player" :
-		print("Player")
+		player = get_node(")../Player2/Player")
+		var direction = (player.position - self.position).normalized()
+		if direction.x > 0:
+			print(str(player.global.positions))
+			print("Right")
