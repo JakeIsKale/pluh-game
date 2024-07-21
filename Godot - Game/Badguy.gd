7f17ad76af1742bba1bba17237af11b7437af979
JakeIsKale <jakeclarkthompson@gmail.com>
2024-07-22 09:29:34 +1200

    Added a portal to travel into a different world, and added some changes to bad guy

--- Godot - Game/Badguy.gd
+++ Godot - Game/Badguy.gd
@@ -12,7 +12,7 @@ func _physics_process(delta):
 func _on_player_detection_body_entered(body):
 	if body.name == "Player" :
 		player = get_node(")../Player2/Player")
-		var direction = (player.position - self.position).normalized()
+		var direction = (player.position = self.position).normalized()
 		if direction.x > 0:
 			print(str(player.global.positions))
 			print("Right")
