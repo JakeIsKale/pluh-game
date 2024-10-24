186baf0bbc75bf69f4bff5370520e8090ec76adc
JakeIsKale <jakeclarkthompson@gmail.com>
2024-10-25 10:03:23 +1300

    boss and world changes

--- Godot - Game/bossdying.gd
+++ Godot - Game/bossdying.gd
@@ -7,3 +7,13 @@ func _ready():
 func _on_idle_end():
 	if $AnimatedSprite2D.animation == "Idle":
 		$AnimatedSprite2D.play("Death")
+
+
+
+func _on_playagain_pressed():
+	get_tree().change_scene_to_file("res://world.tscn")
+
+
+
+func _on_quit_pressed():
+	get_tree().quit()
--- Godot - Game/world2.gd
+++ Godot - Game/world2.gd
@@ -2,3 +2,8 @@ extends Node2D
 
 func _on_deathbutton_pressed():
 	get_tree().change_scene_to_file("res://bossdying.tscn")
+
+
+func _on_deathbutton_2_pressed():
+	get_tree().quit()
+
